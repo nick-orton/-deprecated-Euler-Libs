9e3996f8a59aaf4b6c2f24f3a9e11aac245272b4
Nick Orton <orton.nick@gmail.com>
2010-12-17 21:09:30 -0500

    no test suite yet

--- TestSuite.hs
+++
@@ -1,8 +0,0 @@
-module Euler.TestSuite where
-import Euler.Decimal
-import Euler.Divisors
-import Euler.Lizt
-import Euler.Numz
-import Euler.Primes
-
-testSuite = testDecimal && testDivisors && testLizt && testNumz && testPrimes 
